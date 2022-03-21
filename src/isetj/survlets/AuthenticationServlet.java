c429649dd04614f7f5d5d839fd5663a89c4d4af9
Home <Home@DESKTOP-JUSCQEA.mshome.net>
2022-03-21 20:38:54 +0100

    tp1 partie1

---
+++ src/isetj/survlets/AuthenticationServlet.java
@@ -0,0 +1,50 @@
+package isetj.survlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AuthenticationServlet
+ */
+@WebServlet("/AuthenticationServlet")
+public class AuthenticationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public AuthenticationServlet() {
+    	super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String log = request.getParameter("log");
+		String password = request.getParameter("password");
+		
+		if ((log.equals("dsi")) && (password.equals("dsi"))) {
+			response.getWriter().println("<p style='color:green;'>Authentication réussie</p>");
+			
+		}
+		else {
+			response.getWriter().println("<p style='color:red;'>Echéc à l'authentication</p>");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
